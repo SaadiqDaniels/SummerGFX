cefa0f063e49e31ef02e395ac3e52fbfed76253c
Saadiq <s.daniels@digipen.edu>
2020-04-26 14:54:00 -0700

    Started opengl implementation
    A header and source file for the opengl implementation of the graphics standard

---
+++ OpenGL/src/opengl_standard_gfx.cpp
@@ -0,0 +1,5 @@
+/*!
+ * @author Saadiq Daniels
+ * @file gfx.cpp
+ * @date 4/26/2020
+ */
---
+++ OpenGL/src/opengl_standard_gfx.h
@@ -0,0 +1,12 @@
+/*!
+ * @author Saadiq Daniels
+ * @file gfx.h
+ * @date 26/4/2020
+ */
+
+#ifndef OPENGL_STANDARD_GFX_H
+#define OPENGL_STANDARD_GFX_H
+
+#include "gfx.h"
+
+#endif //OPENGL_STANDARD_GFX_H
